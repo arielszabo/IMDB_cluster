266b06479c2405cb74c668bbbf1fba8e6a876e0d
Ariel Szabo <arielszabo@gmail.com>
2018-08-09 14:56:59 +0300

    refactor the table creation to one func

--- SQL_DB.py
+++ SQL_DB.py
@@ -11,68 +11,56 @@ class DB(object):
         self.create_DB()
 
     # the Ratings column is out because its more than one row and we make a differ table for it
-    def _create_main_table(self):
+    def _create_table(self, table_name, table_columns):
         with sqlite3.connect(self.db_name) as conn:
             c = conn.cursor()
 
             try:
-                c.execute('''CREATE TABLE movies (
+                c.execute('''CREATE table IF NOT EXISTS {} (
                     id PRIMARY KEY,
-                    Actors VARCRCHAR(400),
-                    Awards VARCRCHAR(400),
-                    BoxOffice VARCRCHAR(400),
-                    Country VARCRCHAR(400),
-                    DVD VARCRCHAR(400),
-                    Director VARCRCHAR(400),
-                    Genre VARCRCHAR(400),
-                    Language VARCRCHAR(400),
-                    Metascore DOUBLE,
-                    Plot VARCRCHAR(400),
-                    Poster VARCRCHAR(400),
-                    Production VARCRCHAR(400),
-                    Rated VARCRCHAR(400),
-                    Released VARCRCHAR(400),
-                    Response VARCRCHAR(100),
-                    Runtime VARCRCHAR(400),
-                    Title VARCRCHAR(400),
-                    Type VARCRCHAR(400),
-                    Website VARCRCHAR(400),
-                    Writer VARCRCHAR(400),
-                    Year INTEGER,
-                    imdbID VARCRCHAR(400) UNIQUE NOT NULL,
-                    imdbRating DOUBLE,
-                    imdbVotes VARCRCHAR(400))''')
+                    {})'''.format(table_name, table_columns))
+                print('''CREATE table IF NOT EXISTS {} (
+                    id PRIMARY KEY,
+                    {})'''.format(table_name, table_columns))
             except sqlite3.OperationalError as e:
-                print('sqlite error:', e.args[0])  # table companies already exists
+                print('sqlite error:', e.args[0])
 
             conn.commit()
-            print('Created the movies table')
-
-    def _create_rating_table(self):
-        with sqlite3.connect(self.db_name) as conn:
-            c = conn.cursor()
-
-            try:
-                c.execute('''CREATE TABLE Ratings (
-                    id PRIMARY KEY,
-                    imdbID VARCRCHAR(400) NOT NULL,
-                    Value VARCRCHAR(100),
-                    Source VARCRCHAR(100))
-                    ''')
-            except sqlite3.OperationalError as e:
-                print('sqlite error:', e.args[0])  # table companies already exists
-
-            conn.commit()
-            print('Created the Ratings table')
 
     def create_DB(self):
-        if os.path.exists(self.db_name):
-          print('Great the {} is exists'.format(self.db_name))
-          #todo: check if all the tables exist
-          #todo: check that all the json raw data is in and if not insert it
-        else:
-            self._create_main_table()
-            self._create_rating_table()
+        tables_creation_params = [
+            {'table_name':'movies', 'table_columns':'''Actors VARCRCHAR(400), 
+                                                        Awards VARCRCHAR(400),
+                                                        BoxOffice VARCRCHAR(400),
+                                                        Country VARCRCHAR(400),
+                                                        DVD VARCRCHAR(400),
+                                                        Director VARCRCHAR(400),
+                                                        Genre VARCRCHAR(400),
+                                                        Language VARCRCHAR(400),
+                                                        Metascore DOUBLE,
+                                                        Plot VARCRCHAR(400),
+                                                        Poster VARCRCHAR(400),
+                                                        Production VARCRCHAR(400),
+                                                        Rated VARCRCHAR(400),
+                                                        Released VARCRCHAR(400),
+                                                        Response VARCRCHAR(100),
+                                                        Runtime VARCRCHAR(400),
+                                                        Title VARCRCHAR(400),
+                                                        Type VARCRCHAR(400),
+                                                        Website VARCRCHAR(400),
+                                                        Writer VARCRCHAR(400),
+                                                        Year INTEGER,
+                                                        imdbID VARCRCHAR(400) UNIQUE NOT NULL,
+                                                        imdbRating DOUBLE,
+                                                        imdbVotes VARCRCHAR(400)'''},
+            {'table_name': 'ratings', 'table_columns': '''imdbID VARCRCHAR(400) NOT NULL,
+                                                            Value VARCRCHAR(100),
+                                                            Source VARCRCHAR(100)'''},
+        ]
+        for table_params in tables_creation_params:
+            self._create_table(**table_params)
+
+        # Insert raw_data
 
     def _insert_data(self,data, table):
         with sqlite3.connect(self.db_name) as conn:
@@ -135,5 +123,3 @@ class DB(object):
 
 if __name__ == '__main__':
     db = DB('imdb_test.db')
-    # db.prepare_data_and_insert()
-    print(db._already_in_db())
