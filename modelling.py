fe851c311b36e284ea9e8c5660b360d3e7a2198e
arielszabo <arielszabo@gmail.com>
2018-09-23 20:24:06 +0300

    try to do it without the sql step

--- modelling.py
+++ modelling.py
@@ -1,15 +1,55 @@
 import sqlalchemy
 import os
 import re
+import json
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
 
+class data_loader(object):
+    def __init__(self):
+        self.movies_n_series, self.ratings = self.load_imdb()
+        self.wiki = self.load_wiki()
+
+    @staticmethod
+    def load_imdb():
+        path_for_movie_jsons = 'raw_data'
+        movies_n_series = []
+        ratings = []
+        for file in os.listdir(path_for_movie_jsons):
+            if '.json' in file and not ('!' in file or '?' in file):
+                full_file_path = os.path.join(path_for_movie_jsons, file)
+                with open(full_file_path, 'r') as jfile:
+                    movie_details = json.load(jfile)
+
+                if 'Ratings' in movie_details:
+                    for rating in movie_details['Ratings']:
+                        rating['imdbID'] = movie_details['imdbID']
+
+                    ratings += movie_details.pop('Ratings')
+                movies_n_series.append(movie_details)
+
+        return pd.DataFrame(movies_n_series), pd.DataFrame(ratings)
+
+    @staticmethod
+    def load_wiki():
+        wiki = []
+        path_for_wiki_jsons = os.path.join('raw_data', 'wiki_data')
+        for file in os.listdir(path_for_wiki_jsons):
+            if '.json' in file and not ('!' in file or '?' in file):
+                full_file_path = os.path.join(path_for_wiki_jsons, file)
+                with open(full_file_path, 'r') as jfile:
+                    wiki.append(json.load(jfile))
+
+        return pd.DataFrame(wiki)
+
+
 class MovieVectorization(object):
-    def __init__(self, db_name):
-        self.sql_engine = sqlalchemy.create_engine('sqlite:///{}'.format(os.path.join(os.getcwd(), db_name)))
-        self.movies_table = pd.read_sql("""select * from movies""", con=self.sql_engine)
+    def __init__(self, movies_n_series_df, rating_df, wiki_df):
+        self.movies_n_series_table = movies_n_series_df
+        self.ratings = rating_df
+        self.wiki_data = wiki_df
 
     @staticmethod
     def extract_win_and_nominated_awards(awards_string):
@@ -33,45 +73,50 @@ class MovieVectorization(object):
 
     def movies_cleaning(self):
         # drop unrelevant columns
-        self.movies_table.drop(['id', 'DVD', 'Website', 'Response', 'Poster', 'Released'], axis=1, inplace=True)
+        self.movies_n_series_table.drop(['id', 'DVD', 'Website', 'Response', 'Poster', 'Released'], axis=1, inplace=True)
         # conver to nan
-        self.movies_table.replace('N/A', np.nan, inplace=True)
+        self.movies_n_series_table.replace('N/A', np.nan, inplace=True)
 
-        self.movies_table['imdbVotes'] = self.movies_table['imdbVotes'].str.replace(',', '').astype(float)
+        self.movies_n_series_table['imdbVotes'] = self.movies_n_series_table['imdbVotes'].str.replace(',', '').astype(float)
 
-        box_office_pound = self.movies_table.dropna()[~self.movies_table['BoxOffice'].dropna().str.contains('\$')]
+        box_office_pound = self.movies_n_series_table.dropna()[~self.movies_n_series_table['BoxOffice'].dropna().str.contains('\$')]
 
-        self.movies_table['BoxOffice'] = self.movies_table.BoxOffice.str.replace('\D', '').replace('', np.nan)
-        self.movies_table['BoxOffice'] = self.movies_table['BoxOffice'].astype(float)
-        self.movies_table.loc[box_office_pound.index, 'BoxOffice'] *= 1.3
+        self.movies_n_series_table['BoxOffice'] = self.movies_n_series_table.BoxOffice.str.replace('\D', '').replace('', np.nan)
+        self.movies_n_series_table['BoxOffice'] = self.movies_n_series_table['BoxOffice'].astype(float)
+        self.movies_n_series_table.loc[box_office_pound.index, 'BoxOffice'] *= 1.3
 
-        self.movies_table['Year'] = self.movies_table['Year'].apply(lambda years:
+        self.movies_n_series_table['Year'] = self.movies_n_series_table['Year'].apply(lambda years:
                                                                     np.mean([float(year)
                                                                              for year in str(years).split(r'–')])
-                                                                    )
+                                                                                      )
 
-        self.movies_table['Runtime'] = self.movies_table.Runtime.str.extract('(\d+)', expand=True).astype(float)
+        self.movies_n_series_table['Runtime'] = self.movies_n_series_table.Runtime.str.extract('(\d+)', expand=True).astype(float)
 
     def movies_extract_features(self):
-        self.movies_table['Awards_wins'], self.movies_table['Awards_nominate'] =\
-            zip(*self.movies_table['Awards'].apply(self.extract_win_and_nominated_awards))
+        self.movies_n_series_table['Awards_wins'], self.movies_n_series_table['Awards_nominate'] =\
+            zip(*self.movies_n_series_table['Awards'].apply(self.extract_win_and_nominated_awards))
 
-        self.movies_table.drop('Awards', axis=1, inplace=True)
+        self.movies_n_series_table.drop('Awards', axis=1, inplace=True)
 
         for column_name in ['Country', 'Director', 'Genre', 'Language', 'Actors', 'Production', 'Writer']:
             if column_name == 'Director':  # There are some co-directors which is noted with perentesis
-                self.movies_table['Director'] = self.movies_table['Director'].str.replace('\(.+\)', '')
-                self.movies_table = self.movies_table.join(self.extract_from_comma_sperated_strings(self.movies_table,
-                                                                                                    column_name))
-            self.movies_table.drop(column_name, axis=1, inplace=True)
+                self.movies_n_series_table['Director'] = self.movies_n_series_table['Director'].str.replace('\(.+\)', '')
+                self.movies_n_series_table = self.movies_n_series_table.join(self.extract_from_comma_sperated_strings(self.movies_n_series_table,
+                                                                                                                      column_name))
+            self.movies_n_series_table.drop(column_name, axis=1, inplace=True)
 
-        return self.movies_table
+        return self.movies_n_series_table
 
 
 if __name__ == '__main__':
-    movies = MovieVectorization(db_name='imdb_test.db')
+    dfs = {
+        'movies_n_series_df': data_loader().movies_n_series,
+        'rating_df': data_loader().ratings,
+        'wiki_df': data_loader().wiki
+         }
+    movies = MovieVectorization(**dfs)
     movies.movies_cleaning()
     movies.movies_extract_features()
-    print(movies.movies_table.head())
+    print(movies.movies_n_series_table.head())
 
     # movies_after_feature_extraction.to_sql(name='movies_extracted', con=engine)
